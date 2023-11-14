5d36a60f5d92b1ca3e2fcd1684afa4980a301e0d
egjallow10 <egjallow10@gmail.com>
2023-11-14 16:15:26 +0000

    formart a string

---
+++ print_formar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_format - check format and prints
+ * @specifier: format string type
+ * @a_list: list of arguments
+ * Return: return an int.
+ */
+
+int print_format(char specifier, va_list a_list)
+{
+}
