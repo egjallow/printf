3961232d7f5356ba3dd3d0ce471c6ce74f3ce2b6
egjallow10 <egjallow10@gmail.com>
2023-11-14 15:48:00 +0000

    intiate the printf

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - is a function that prints
+ * @format: the format string
+ * @...: the arguments
+ * Return: the result shold be int.
+ */
+int _printf(const char *format, ...)
+{
+}
